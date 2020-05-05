418c280c73efe10ff4ef48da3a2429a44aae9d83
Kamil M <k.mikolajczuk123@gmail.com>
2020-05-05 21:43:25 +0200

    defaults

--- src/NewGraph/defaultGraphs.ts
+++ src/NewGraph/defaultGraphs.ts
@@ -3,6 +3,15 @@ export interface IAvailableGraph {
   graph: number[][]
 }
 
+const randomGraph = (n: number, successRatio = 0.2) =>
+  Array(n)
+    .fill(0)
+    .map((_, i) =>
+      Array(n)
+        .fill(0)
+        .map((_, j) => (i === j || Math.random() >= successRatio ? 0 : 1))
+    )
+
 const buildInGraphs: IAvailableGraph[] = [
   {
     name: 'Klasyka gatunku graf z Wikipedii',
@@ -16,12 +25,34 @@ const buildInGraphs: IAvailableGraph[] = [
     ],
   },
   {
+    name: 'Nieco bardziej skomplikowany graf',
+    graph: [
+      [0, 0, 1, 1, 0],
+      [1, 0, 1, 1, 1],
+      [0, 1, 0, 1, 1],
+      [1, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0],
+    ],
+  },
+  {
     name: '3 wierzchołkowy wielokąt foremny',
     graph: [
       [0, 1, 1],
       [1, 0, 1],
       [1, 1, 0],
     ],
+  },
+  {
+    name: 'Losowy graf 5 wierzchołków',
+    graph: randomGraph(5, 0.5),
+  },
+  {
+    name: 'Losowy graf 10 wierzchołków',
+    graph: randomGraph(10, 0.2),
+  },
+  {
+    name: 'Losowy graf 20 wierzchołków',
+    graph: randomGraph(20, 0.12),
   },
 ]
 
